453d46b23c6c0fb9c4afba5da77ddd6c77fb87e0
Pei Ern Koh <peiern.28@gmail.com>
2021-11-09 00:48:16 +0800

    able to sort title alphabetically

--- index.js
+++ index.js
@@ -1,31 +1,49 @@
 const lists = document.querySelector('.lists');
 
 function showList(){
+  // call the API
   fetch('https://jsonplaceholder.typicode.com/posts')
     .then(res => res.json())
     .then(data => {
-
+      // go through each of the array
+      // get the title, body and user ID
       data.forEach(post => {
         const postLists = `
         <h3>Title: ${post.title}</h3>
-          ${post.body}
+          <p>${post.body}</p>
+          <p>Posted by: User ${post.userId}</p>
       `;
-
+        // insert into HTML for display
         lists.insertAdjacentHTML('beforeend', postLists);
       });
     });
 };
 
-function sorting() {
-  // fetch('https://jsonplaceholder.typicode.com/posts')
-  //   .then(res => res.json())
-  //   .then(data => {
-  //     data.forEach.sort(post => {
-  //       const sorted = `
-  //       <h3>Title: ${post.title}</h3>
-  //         ${post.body}
-  //     `;
-  //     lists.insertAdjacentHTML('beforeend', sorted);
-  //     });
-  //   });
-};
+function sortByAlphabetically() {
+  // comparer function
+  function sorting(prop) {
+    return function(a, b) {
+      if (a[prop] > b[prop]) {
+        return 1;
+      } else if (a[prop] < b[prop]) {
+        return -1;
+      }
+      return 0;
+    }
+  };
+
+  fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(res => res.json())
+    .then(data => {
+
+      data.sort(sorting('title')); // sorting by title property
+      for (const item in data) {
+        const sorted = `
+        <h3>Title: ${data[item].title}</h3>
+        <p>${data[item].body}</p>
+        <p>Posted by: User ${data[item].userId}</p>
+        `;
+        lists.insertAdjacentHTML("beforeend", sorted);
+      };
+    });
+}
